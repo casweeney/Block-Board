b2736f0295e6e1ee39c58bb659d96eefa6104406
CodingCas <casweeno2000@gmail.com>
2022-09-16 05:14:31 +0100

    Init and UI implementation

---
+++ src/components/GasFee.jsx
@@ -0,0 +1,199 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const GasFee = () => {
+    return (
+        <div className="">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark extra-dark">
+            <div className="container">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                    <Link className="nav-link" to="/">Transactions <span className="sr-only">(current)</span></Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/protocols">Protocols</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/staking-reports">Staking Reports</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/nft-collections">NFT & Collections</Link>
+                    </li>
+
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/gas-fee">Gas Fee Estimate</Link>
+                    </li>
+                </ul>
+
+                <form className="form-inline">
+                    <div className="input-group">
+                    <input type="text" className="form-control bg-dark-gray text-white" placeholder="Enter protocol name" aria-label="protocol-name" aria-describedby="basic-addon1" />
+                    <div className="input-group-append">
+                        <button className="input-group-text" id="basic-addon1">Lookup</button>
+                    </div>
+                    </div>
+                </form>
+                </div>
+            </div>
+            </nav>
+
+            <main className="extra-dark pb-4 main">
+            <div className="container">
+                <h3 className="text-danger pt-4 pb-2">Protocol Gas Fee Estimate</h3>
+                <div className="row">
+                <div className="col-md-4 mb-4">
+                    <div className="card bg-dark">
+                    <div className="card-body">
+                        <h6 class="text-white mb-3">Cardano Coin Fee Estimate</h6>
+
+                        <div className="main-flex">
+                        <div class="icon-text-holder">
+                            <img class="pr-1" src="https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png" alt="" />
+                            <div className="mr-5">
+                            <p className="text-white mini-text">Cardano</p>
+                            <p className="text-white mini-text">Fast: 8555</p>
+                            <p className="text-white mini-text">Medium: 8243</p>
+                            <p className="text-white mini-text">Slow: 7562</p>
+                            </div>
+                        </div>
+                        <svg width="109" height="34" viewBox="0 0 109 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 3.51685L3.79739 6.03371H5.54575L7.99346 8.19101L11.1405 6.03371L13.5882 10.3483L14.9869 3.51685L16.3856 6.03371L18.134 3.51685V8.19101L21.9804 1L24.7778 10.3483L26.8758 8.19101L28.6242 17.1798L31.4216 13.9438L33.5196 25.4494L35.6176 17.1798L37.7157 20.0562L39.1144 17.1798L40.8627 20.0562L44.7092 13.9438L47.1569 17.1798L50.3039 13.9438L54.1503 20.0562L56.598 17.1798L60.7941 25.4494L62.8922 23.6517L65.3399 28.3258L67.0882 25.4494L69.8856 33L75.8301 20.0562L77.9281 21.8539L80.0261 15.382L81.7745 17.1798L89.4673 6.03371L91.915 10.3483L94.7124 6.03371L97.5098 10.3483L99.6078 8.19101L105.203 17.1798L108 13.9438" stroke="#16C784" stroke-width="2" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+
+                    </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4 mb-4">
+                    <div className="card bg-dark">
+                    <div className="card-body">
+                        <h6 class="text-white mb-3">Doge Coin Fee Estimate</h6>
+
+                        <div className="main-flex">
+                        <div class="icon-text-holder">
+                            <img class="pr-1" src="https://s2.coinmarketcap.com/static/img/coins/64x64/74.png" alt="" />
+                            <div className="mr-5">
+                            <p className="text-white mini-text">DogeCoin</p>
+                            <p className="text-white mini-text">Fast: 8555</p>
+                            <p className="text-white mini-text">Medium: 8243</p>
+                            <p className="text-white mini-text">Slow: 7562</p>
+                            </div>
+                        </div>
+                        <svg width="111" height="43" viewBox="0 0 111 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 4.22472L3.84967 7.44944H5.63072L8.12418 10.2135L11.3301 7.44944L13.8235 12.9775L15.2484 4.22472L16.6732 7.44944L18.4542 4.22472V10.2135L22.3725 1L25.2222 12.9775L27.3595 10.2135L29.1405 21.7303L31.9902 17.5843L34.1275 32.3258L36.2647 21.7303L38.402 25.4157L39.8268 21.7303L41.6078 25.4157L45.5261 17.5843L48.0196 21.7303L51.2255 17.5843L55.1438 25.4157L57.6373 21.7303L61.9118 32.3258L64.049 30.0225L66.5425 36.0112L68.3235 32.3258L71.1732 42L77.2288 25.4157L79.366 27.7191L81.5033 19.427L83.2843 21.7303L91.1209 7.44944L93.6144 12.9775L96.4641 7.44944L99.3137 12.9775L101.451 10.2135L107.15 21.7303L110 17.5843" stroke="#EA3943" stroke-width="2" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+
+                    </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4 mb-4">
+                    <div className="card bg-dark">
+                    <div className="card-body">
+                        <h6 class="text-white mb-3">Polkadot Coin Fee Estimate</h6>
+
+                        <div className="main-flex">
+                        <div class="icon-text-holder">
+                            <img class="pr-1" src="https://s2.coinmarketcap.com/static/img/coins/64x64/6636.png" alt="" />
+                            <div className="mr-5">
+                            <p className="text-white mini-text">Polkadot</p>
+                            <p className="text-white mini-text">Fast: 8555</p>
+                            <p className="text-white mini-text">Medium: 8243</p>
+                            <p className="text-white mini-text">Slow: 7562</p>
+                            </div>
+                        </div>
+                        <svg width="111" height="43" viewBox="0 0 111 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 4.22472L3.84967 7.44944H5.63072L8.12418 10.2135L11.3301 7.44944L13.8235 12.9775L15.2484 4.22472L16.6732 7.44944L18.4542 4.22472V10.2135L22.3725 1L25.2222 12.9775L27.3595 10.2135L29.1405 21.7303L31.9902 17.5843L34.1275 32.3258L36.2647 21.7303L38.402 25.4157L39.8268 21.7303L41.6078 25.4157L45.5261 17.5843L48.0196 21.7303L51.2255 17.5843L55.1438 25.4157L57.6373 21.7303L61.9118 32.3258L64.049 30.0225L66.5425 36.0112L68.3235 32.3258L71.1732 42L77.2288 25.4157L79.366 27.7191L81.5033 19.427L83.2843 21.7303L91.1209 7.44944L93.6144 12.9775L96.4641 7.44944L99.3137 12.9775L101.451 10.2135L107.15 21.7303L110 17.5843" stroke="#EA3943" stroke-width="2" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+
+                    </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4 mb-4">
+                    <div className="card bg-dark">
+                    <div className="card-body">
+                        <h6 class="text-white mb-3">Ethereum Coin Fee Estimate</h6>
+
+                        <div className="main-flex">
+                        <div class="icon-text-holder">
+                            <img class="pr-1" src="https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png" alt="" />
+                            <div className="mr-5">
+                            <p className="text-white mini-text">Ethereum</p>
+                            <p className="text-white mini-text">Fast: 8555</p>
+                            <p className="text-white mini-text">Medium: 8243</p>
+                            <p className="text-white mini-text">Slow: 7562</p>
+                            </div>
+                        </div>
+                        <svg width="111" height="43" viewBox="0 0 111 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 4.22472L3.84967 7.44944H5.63072L8.12418 10.2135L11.3301 7.44944L13.8235 12.9775L15.2484 4.22472L16.6732 7.44944L18.4542 4.22472V10.2135L22.3725 1L25.2222 12.9775L27.3595 10.2135L29.1405 21.7303L31.9902 17.5843L34.1275 32.3258L36.2647 21.7303L38.402 25.4157L39.8268 21.7303L41.6078 25.4157L45.5261 17.5843L48.0196 21.7303L51.2255 17.5843L55.1438 25.4157L57.6373 21.7303L61.9118 32.3258L64.049 30.0225L66.5425 36.0112L68.3235 32.3258L71.1732 42L77.2288 25.4157L79.366 27.7191L81.5033 19.427L83.2843 21.7303L91.1209 7.44944L93.6144 12.9775L96.4641 7.44944L99.3137 12.9775L101.451 10.2135L107.15 21.7303L110 17.5843" stroke="#EA3943" stroke-width="2" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+
+                    </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4 mb-4">
+                    <div className="card bg-dark">
+                    <div className="card-body">
+                        <h6 class="text-white mb-3">Bitcoin Coin Fee Estimate</h6>
+
+                        <div className="main-flex">
+                        <div class="icon-text-holder">
+                            <img class="pr-1" src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png" alt="" />
+                            <div className="mr-5">
+                            <p className="text-white mini-text">Bitcoin</p>
+                            <p className="text-white mini-text">Fast: 8555</p>
+                            <p className="text-white mini-text">Medium: 8243</p>
+                            <p className="text-white mini-text">Slow: 7562</p>
+                            </div>
+                        </div>
+                        <svg width="109" height="34" viewBox="0 0 109 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 3.51685L3.79739 6.03371H5.54575L7.99346 8.19101L11.1405 6.03371L13.5882 10.3483L14.9869 3.51685L16.3856 6.03371L18.134 3.51685V8.19101L21.9804 1L24.7778 10.3483L26.8758 8.19101L28.6242 17.1798L31.4216 13.9438L33.5196 25.4494L35.6176 17.1798L37.7157 20.0562L39.1144 17.1798L40.8627 20.0562L44.7092 13.9438L47.1569 17.1798L50.3039 13.9438L54.1503 20.0562L56.598 17.1798L60.7941 25.4494L62.8922 23.6517L65.3399 28.3258L67.0882 25.4494L69.8856 33L75.8301 20.0562L77.9281 21.8539L80.0261 15.382L81.7745 17.1798L89.4673 6.03371L91.915 10.3483L94.7124 6.03371L97.5098 10.3483L99.6078 8.19101L105.203 17.1798L108 13.9438" stroke="#16C784" stroke-width="2" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+
+                    </div>
+                    </div>
+                </div>
+
+                <div className="col-md-4 mb-4">
+                    <div className="card bg-dark">
+                    <div className="card-body">
+                        <h6 class="text-white mb-3">Ripple Coin Fee Estimate</h6>
+
+                        <div className="main-flex">
+                        <div class="icon-text-holder">
+                            <img class="pr-1" src="https://s2.coinmarketcap.com/static/img/coins/64x64/52.png" alt="" />
+                            <div className="mr-5">
+                            <p className="text-white mini-text">Ripple</p>
+                            <p className="text-white mini-text">Fast: 8555</p>
+                            <p className="text-white mini-text">Medium: 8243</p>
+                            <p className="text-white mini-text">Slow: 7562</p>
+                            </div>
+                        </div>
+                        <svg width="109" height="34" viewBox="0 0 109 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 3.51685L3.79739 6.03371H5.54575L7.99346 8.19101L11.1405 6.03371L13.5882 10.3483L14.9869 3.51685L16.3856 6.03371L18.134 3.51685V8.19101L21.9804 1L24.7778 10.3483L26.8758 8.19101L28.6242 17.1798L31.4216 13.9438L33.5196 25.4494L35.6176 17.1798L37.7157 20.0562L39.1144 17.1798L40.8627 20.0562L44.7092 13.9438L47.1569 17.1798L50.3039 13.9438L54.1503 20.0562L56.598 17.1798L60.7941 25.4494L62.8922 23.6517L65.3399 28.3258L67.0882 25.4494L69.8856 33L75.8301 20.0562L77.9281 21.8539L80.0261 15.382L81.7745 17.1798L89.4673 6.03371L91.915 10.3483L94.7124 6.03371L97.5098 10.3483L99.6078 8.19101L105.203 17.1798L108 13.9438" stroke="#16C784" stroke-width="2" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </main>
+        </div>
+    );
+}
+
+export default GasFee;
